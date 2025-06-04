8b5e90a6bf6f3c6e4fd402e2c09cd113130bdc15
scvhea <svea.holm03@gmail.com>
2025-06-04 14:49:00 +0200

    middleware o  koppling till schema

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,14 @@ const express = require('express');
 const axios = require('axios');
 require('dotenv').config();
 
+const Recipe = require('./models/spoon');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(require('cors')());
+
+
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
