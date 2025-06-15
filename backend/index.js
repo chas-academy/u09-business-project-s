c1520a77f6d3af00b25e8fae021a0051635a6330
scvhea <svea.holm03@gmail.com>
2025-06-15 21:29:21 +0200

    sista igen

--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,13 @@ const corsOptions = {
   credentials: true,    
 };
 app.use(cors(corsOptions));
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
